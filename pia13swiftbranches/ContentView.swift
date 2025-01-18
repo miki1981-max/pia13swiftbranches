a08b5e66d98fa428653de91543787fc79fd3636b
miki1981-max <miki43154@gmail.com>
2025-01-18 20:25:27 +0100

    Initial Commit

---
+++ pia13swiftbranches/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pia13swiftbranches
+//
+//  Created by Kamila Ponomarova on 2025-01-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pia13swiftbranches/pia13swiftbranchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  pia13swiftbranchesApp.swift
+//  pia13swiftbranches
+//
+//  Created by Kamila Ponomarova on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct pia13swiftbranchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
